4b0d35a2db366331de58dddcfbf5a29350ce8092
Toby Thain <toby.thain@fivebox.com>
2026-02-23 02:23:18 -0500

    Add Haskell solution.

--- cses/01_introductory/01_weird_algorithm/main.hs
+++ cses/01_introductory/01_weird_algorithm/main.hs
@@ -1,5 +1,16 @@
--- main :: IO()
--- main = do
---     line <- getLine
---     let [a, b] = map read (words line)
---     print (a + b)
+-- import Numeric (readInt)
+
+
+main :: IO ()
+
+step :: Int -> IO ()
+
+step 1 = print 1
+step x | even x = print x >>= (\_ -> step (x `div` 2))
+       | otherwise = print x >>= (\_ -> step ((x * 3) + 1))
+
+
+main = do
+  line <- getLine
+  let n::Int = read line
+  step n
