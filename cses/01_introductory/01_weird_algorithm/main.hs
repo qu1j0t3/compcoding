e1db21a1a0ed927813da08955ed89140fcc4d450
Toby Thain <toby.thain@fivebox.com>
2026-02-23 11:17:38 -0500

    Simplify.

--- cses/01_introductory/01_weird_algorithm/main.hs
+++ cses/01_introductory/01_weird_algorithm/main.hs
@@ -1,16 +1,10 @@
--- import Numeric (readInt)
+step :: Int -> IO ()
+
+step 1 = print 1
+step x | even x    = print x >>= (\_ -> step (x `div` 2))
+       | otherwise = print x >>= (\_ -> step ((x * 3) + 1))
 
 
 main :: IO ()
 
-step :: Int -> IO ()
-
-step 1 = print 1
-step x | even x = print x >>= (\_ -> step (x `div` 2))
-       | otherwise = print x >>= (\_ -> step ((x * 3) + 1))
-
-
-main = do
-  line <- getLine
-  let n::Int = read line
-  step n
+main = getLine >>= (\line -> step (read line))
