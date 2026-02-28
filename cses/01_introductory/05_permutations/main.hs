07959a472e3537eca578a80145537a19f8a594ed
Toby Thain <toby.thain@fivebox.com>
2026-02-28 02:26:36 -0500

    Fix error.

--- cses/01_introductory/05_permutations/main.hs
+++ cses/01_introductory/05_permutations/main.hs
@@ -7,12 +7,11 @@
 perm :: Int -> String
 
 perm count = let half = (count + 1) `div` 2
-                 left = take half [1,3..]
-                 lastItem = 2*half-1 -- the last item of `left`
+                 left = take half [2,4..]
                  -- if lastItem is less than 4, then the joined lists
                  -- will have a difference of < 2, making a non-beautiful permutation
-                 right = take (count - half) [2,4..]
-             in if lastItem < 4
+                 right = take (count - half) [1,3..]
+             in if count == 2 || count == 3
                 then "NO SOLUTION"
                 else unwords (map show (left ++ right))
 
