b5af0bd9a7c3146b54db685f8c456a0e7a2c8cf2
Toby Thain <toby.thain@fivebox.com>
2026-03-01 15:09:41 -0500

    Add solution.

---
+++ cses/01_introductory/06_number_spiral/main.hs
@@ -0,0 +1,33 @@
+-- Copyright (C) 2026 Toby Thain, toby@telegraphics.net
+
+-- Solution to CSES Introductory Problems Number Spiral
+-- https://cses.fi/problemset/task/1071
+
+-- import Text.Printf
+
+
+test [y,x] = getIndex y x
+
+
+runTests :: Int -> IO ()
+
+runTests count = sequence_ [ getLine >>= (print . test . (map read) . words) | t <- [1..count]]
+
+
+getIndex :: Int -> Int -> Int
+
+getIndex y x = let rank = max y x
+                   (y',x') = if odd rank then (y,x) else (x,y)
+               in rank*rank - (if y' > x' then 2*rank-1-x' else y'-1)
+
+
+-- For testing
+printSpiral :: Int -> IO ()
+printSpiral size = sequence_ [ putStrLn (show [getIndex r c | c <- [1..size]]) | r <- [1..size] ]
+
+
+main :: IO ()
+
+main = getLine >>= (runTests . read)
+
+
