daafa93a0bbcad217ba205afc9cef2c66f9deb8e
Toby Thain <toby.thain@fivebox.com>
2026-03-08 22:36:05 -0400

    Add solution.

--- cses/01_introductory/07_two_knights/main.hs
+++ cses/01_introductory/07_two_knights/main.hs
@@ -5,17 +5,36 @@
 
 -- My first result was wrong but led me to the Ball Hating Monster https://oeis.org/A104188
 
+-- The correct sequence is (spoilers): https://oeis.org/A172132
 
 board :: Int -> [Int]
 
 board maxk =
-  [ let moves::[Int] = [1 | r <- [1..k], c <- [1..k],   -- first knight is placed on every board square
-                            a <- [-2..2], b <- [-2..2], -- the 5x5 neighbourhood around it
-                            abs a + abs b /= 3,         -- exclude the 2nd knight threat positions
-                            a /= 0 || b /= 0,           -- exclude position of first knight
-                            r+a >= 1, r+a <= k, c+b >= 1, c+b <= k] -- exclude locations outside board
+  [ let moves::[Int] = [1 | r <- [1..k], c <- [1..k],     -- first knight can be placed on every board square
+                            r' <- [1..k], c' <- [1..k],   -- second knight
+                            (r',c') /= (r,c),             -- can't be same position
+                            abs (r'-r) + abs (c'-c) /= 3 || abs (r'-r) > 2 || abs (c'-c) > 2] -- can't threaten first knight (one knight move away)
     in (length moves) `div` 2
     | k <- [1..maxk] ]
+        -- IMPORTANT NOTE FOR FUTURE
+        -- I spent far too long here not seeing that 'abs (r'-r) + abs (c'-c) /= 3'
+        -- includes moves outside the 5x5 neighbourhood!
+        -- e.g. 3 squares directly horizontal or vertical.
+        -- I had convinced myself this logic was correct partly because
+        -- I only tested on a 5x5 generator e.g. [-2..2]x[-2..2] !
+
+-- Input:
+-- 8
+
+-- Output:
+-- 0
+-- 6
+-- 28
+-- 96
+-- 252
+-- 550
+-- 1056
+-- 1848
 
 
 main :: IO ()
