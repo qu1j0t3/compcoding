100a0c354c0fc33b366a44ddd7bf642f1bf75a63
Toby Thain <toby.thain@fivebox.com>
2026-04-19 17:14:08 -0400

    Simplify.

--- cses/01_introductory/17_queens/main.hs
+++ cses/01_introductory/17_queens/main.hs
@@ -32,43 +32,24 @@
 -- |compute how many non-attacking queens can be placed on 8x8 board.
 
 solve :: [[Int]] -> Int
-solve input =
-  step [] input 8
-  where
-    step :: [(Int,Int)] -> [[Int]] -> Int -> Int
-    step _ _ 0 = 1 -- base case: placed all rows, so count one solution
-    step placed blocked r =
-      -- place Q in each possible column of row r and return sum of each resulting subproblem (r-1)
-      let rowBlocked = blocked !! (r-1)
-      in sum [ let updatedBlocked = -- update unplaced rows (< r) with new threatened columns
-                    map (\ (r',cs) -> let d = abs (r-r')
-                                      in c:(c+d):(c-d):cs)
-                        (zip [1..] (take (r-1) blocked))
-               in step ((r,c):placed) updatedBlocked (r-1)
-               | c <- [1..8], not (elem c rowBlocked) ]
+solve [] = 1            -- base case: placed all rows, so count one solution
+solve (b:bs) =          -- place Q in each possible column of row r and return sum of resulting subproblems (r-1)
+  sum [ let blocked' =  -- update unplaced rows (< r) with new threatened columns
+              map (\ (d,cs) -> c:(c+d):(c-d):cs)
+                  (zip [1..] bs)
+        in solve blocked'
+        | c <- [1..8], not (elem c b) ]
 
---  sum [ if reserved!(i,j) then 1 else 0 | i <- [1..8], j <- [1..8] ] -- just count reserved squares
-
--- We don't use this, but kept as reference
---
--- readInputAssocList :: IO [((Int,Int),Bool)]
--- readInputAssocList =
---   fmap mconcat -- flatten the 2-D input into a list of index/value tuples
---        (traverse (\ r -> -- traverse over 8 rows
---                  fmap (\ line -> map (\ (c,b) -> ((r,c),b)) -- pair (row,col) tuples with reserved flags
---                                      (zip [1..] (map (== '*') line))) -- add column index and test for '*'
---                       getLine) -- read each row as a line (string)
---                  [1..8])
 
 readInput :: IO [[Int]]
-readInput =
-  traverse (\ _ -> -- traverse over rows
-                 fmap (\ line -> concatMap readFlag (zip [1..] line))
-                      getLine) -- read each row as a line (string)
-                 [1::Int .. 8]
+readInput = traverse (\ _ -> -- traverse over rows
+                        fmap (\ line -> concatMap readFlag (zip [1..] line))
+                              getLine) -- read each row as a line (string)
+                        [1::Int .. 8]
   where readFlag :: (Int,Char) -> [Int]
         readFlag (c,'*') = [c]
         readFlag _ = []
+
 
 main :: IO ()
 main = readInput >>= (print . solve)
