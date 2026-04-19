364ab76b3bb126fd60219518ef52a53515ea5536
Toby Thain <toby.thain@fivebox.com>
2026-04-19 13:16:27 -0400

    Just comments.

--- cses/01_introductory/16_apple_division/main.hs
+++ cses/01_introductory/16_apple_division/main.hs
@@ -38,19 +38,13 @@
 
 -- We need to be efficient for problems up to 20 apples (2^20 arrangements)
 
-
--- there is a clever way of getting 'sum of subset'
--- See Knapsack Problems, page 83, Competitive Programmer’s Handbook, Laaksonen
-
 {-
-
 let t = total divide by 2
 look at possible sums
 get the two closest to t
 
   test data: [10,16,2,19,6,13,7,13,17,1]
   [10,2,1,2,3,1,1,5,4]
-
 -}
 
 solve :: [Int] -> Int
@@ -63,10 +57,10 @@ solve xs = let halfDown = (sum xs) `div` 2
 lsum :: [Int] -> Int -> (Int,Int)
 lsum input mx =
   lstep input 0 0
-  where lstep [] acc acc2     = (acc,acc2) -- base case, input used up
+  where lstep [] acc acc2 = (acc,acc2)         -- base case, input used up
         lstep (x:xs) acc acc2 =
-          let (a,a2)   = lstep xs acc (x+acc2) -- take element in acc2 and continue
-              (a',a2') = lstep xs (x+acc) acc2 -- take in acc, continue
+          let (a,a2)   = lstep xs acc (x+acc2) -- include element in acc2 and continue
+              (a',a2') = lstep xs (x+acc) acc2 -- include in acc, continue; only evaluated if needed
           in if acc+x > mx || a > a' then (a,a2) else (a',a2')
 
 
