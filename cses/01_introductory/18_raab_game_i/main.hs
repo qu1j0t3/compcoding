cff3759c2c3fe02660cc01020c77ee2e825d3796
Toby Thain <toby.thain@fivebox.com>
2026-04-25 17:57:45 -0400

    Add checking code against CSES solutions file.

--- cses/01_introductory/18_raab_game_i/main.hs
+++ cses/01_introductory/18_raab_game_i/main.hs
@@ -3,6 +3,18 @@
 -- Solution to CSES Introductory Problems Raab Game I
 -- https://cses.fi/problemset/task/3399
 
+{-# LANGUAGE LambdaCase #-}
+
+import System.Environment
+import System.Exit
+import System.IO
+
+import Data.Foldable
+import Data.List
+-- import Data.Maybe
+-- import Data.IntSet (IntSet, fromList)
+-- import qualified Data.IntSet as IntSet
+
 -- Consider a two player game where each player has n cards numbered 1,2,\dots,n. On each turn both players place one of their cards on the table. The player who placed the higher card gets one point. If the cards are equal, neither player gets a point. The game continues until all cards have been played.
 -- You are given the number of cards n and the players' scores at the end of the game, a and b. Your task is to give an example of how the game could have played out.
 -- Input
@@ -47,57 +59,85 @@ i guess we can do this with backtracking search?
 
 -}
 
-import Data.Foldable
-import Data.List
-import Data.Maybe
--- import Data.IntSet (IntSet, fromList)
--- import qualified Data.IntSet as IntSet
-
 
 -- permgen :: Eq a => [a] -> [[a]]
 -- permgen [] = [[]]
 -- permgen xs = concatMap (\ x -> map (x:) (permgen (delete x xs))) xs
 
--- A game is a permutation of one hand
--- played against 1..n
 
--- This takes a search approach but it's too slow for the size of problem in CSES judge; breaks down around 9 cards
 solve :: [Int] -> Maybe [(Int,Int)]
-solve [n, target1, target2] =
-  let draws = n - target1 - target2
-      n' = n - draws
-      drawPlays = [(a,a) | a <- [n'+1 .. n]]
-  in if draws >= 0 && target1 < n' && target2 < n'
-     then fmap (drawPlays ++) $
-            step [1..n'] [1..n'] n' target1 target2 [] -- search possible plays, each player must meet target with cards 1..n'
-     else Nothing
-     where step :: [Int] -> [Int] -> Int -> Int -> Int -> [(Int,Int)] -> Maybe [(Int,Int)]
-           step _ _ 0 0 0 acc = Just acc -- success! this game is a solution
-           step p1 p2 n t1 t2 acc =
-             listToMaybe $ -- we only want one complete game
-               catMaybes [ if a > b then step (delete a p1) (delete b p2) (n-1) (t1-1) t2 ((a,b):acc)
-                                    else step (delete a p1) (delete b p2) (n-1) t1 (t2-1) ((a,b):acc)
-                           | a <- p1, b <- p2, a /= b ]
-
-  -- step [1..n] [1..n] 0 0
-  -- where
-  --   step :: [Int] -> [Int] -> Int -> Int -> [[(Int,Int)]]
-  --   -- solve subproblem given hands for players, target scores
-  --   step h1 h2 t1 t2 =
-  --     let result if t1 < target1 then
-
-    -- step [] [] t1 t2 = []
-    -- step (a:h1) (b:h2) t1 t2 | a > b && t1 < target1 = (a,b) : step h1 h2 (t1+1) t2
-    --                          | b > a && t2 < target2 = (a,b) : step h1 h2 t1 (t2+1)
-    --                          | a == b                = (a,b) : step h1 h2 t1 t2 -- we could try guard t1==target1 && t2==target2
-    --                          | otherwise             = []
+solve [cards, target1, target2] =
+  if cards >= target1 + target2
+  then step (reverse [1..cards]) (reverse [1..cards]) cards target1 target2 []
+  else Nothing
+    where step :: [Int] -> [Int] -> Int -> Int -> Int -> [(Int,Int)] -> Maybe [(Int,Int)]
+          step _  _ 0 0 0 acc = Just acc -- success! this game is a solution
+          step [] _ _ _ _ _   = Nothing -- can't exhaust either hand with non zero targets
+          step _ [] _ _ _ _   = Nothing
+          step p1@(h1:rest1) p2@(h2:rest2) n t1 t2 acc
+            | n > target1 + target2 = -- play all draws first; we assume h1 and h2 are equal by construction above
+              step rest1 rest2 (n-1) t1 t2 ((h1,h2):acc)
+            | t1 > 0    = -- if either t1 > 0 or t2 > 0, we're not finished playing
+              find (<h1) p2 >>= \ lose2 -> step rest1 (delete lose2 p2) (n-1) (t1-1) t2 ((h1,lose2):acc)
+            | otherwise =
+              find (<h2) p1 >>= \ lose1 -> step (delete lose1 p1) rest2 (n-1) t1 (t2-1) ((lose1,h2):acc)
 
 printSolution :: Maybe [(Int,Int)] -> IO ()
 printSolution Nothing     = putStrLn "NO"
 printSolution (Just game) = putStrLn "YES" >> p fst game >> p snd game
   where p f = putStrLn . unwords . (map (show . f))
 
+failWithMessage :: String -> IO ()
+failWithMessage err = hPutStrLn stderr err >> exitWith (ExitFailure 1)
+
+-- Given number of cards and target scores, check whether
+-- the given plays are a valid solution
+
+checkGame :: [Int] -> [(Int,Int)] -> Maybe Bool
+checkGame [n, t1, t2] game =
+  if sort (map fst game) == [1..n] && sort (map snd game) == [1..n]
+  then let scores = map (\(a,b) -> (a>b, a<b)) game
+         in Just $ length (filter fst scores) == t1 && length (filter snd scores) == t2
+  else Nothing
+
+-- Run: ./main < 1.in --test 1.out
+-- problems will be read from input and solved,
+-- then checked against the solutions file given
+
+runWithChecking :: String -> IO ()
+runWithChecking solutionFile =
+  getLine >>= \ line1 ->
+    withFile solutionFile ReadMode
+      ( \ handle ->
+          let n::Int = read line1
+              checkSolution :: [Int] -> Maybe [(Int,Int)] -> IO ()
+              checkSolution input result =
+                hGetLine handle >>= (\ expected ->
+                    case result of
+                      Nothing -> if expected == "NO"
+                                 then putStrLn "  PASS"
+                                 else failWithMessage $ "FAIL: no solution, but expected " ++ expected
+                      Just game -> if expected == "YES"
+                                   then -- skip two solution lines, as we will check ourselves
+                                     hGetLine handle >> hGetLine handle >>
+                                       case checkGame input game of
+                                         Nothing    -> failWithMessage "INVALID PLAYS"
+                                         Just True  -> putStrLn $ "  PASS GAME: " ++ (show game)
+                                         Just False -> failWithMessage "FAIL SCORES"
+                                         -- hPutStrLn stderr sol1 >> hPutStrLn stderr sol2
+                                   else failWithMessage $ "FAIL: expected " ++ expected
+                  )
+          in traverse_ (\ gameNumber -> getLine
+                          >>= (\ l -> fmap (const l) (putStrLn $ "#" ++ (show gameNumber) ++ " Input: " ++ l)) -- print input lines for debugging
+                          >>= ((\ input -> checkSolution input (solve input)) . (map read) . words))
+                        [1..n]
+      )
+
 main :: IO ()
-main = getLine >>= ( games . read )
+main = getArgs >>= ( \case
+            ["--test", solutionFile] -> runWithChecking solutionFile
+            [] -> getLine >>= ( games . read )
+            _ -> failWithMessage "run with no arguments, or --test solutionFile"
+          )
   where games :: Int -> IO ()
         games n = traverse_ (\ _ -> getLine >>= (printSolution . solve . (map read) . words)) [1..n]
