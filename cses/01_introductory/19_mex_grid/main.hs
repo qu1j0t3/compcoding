86e6f9f0602d67080f934c00f5326ece570302fa
Toby Thain <toby.thain@fivebox.com>
2026-05-03 02:10:57 -0400

    Feels like cheating to use the OEIS... Add the real solution.

--- cses/01_introductory/19_mex_grid/main.hs
+++ cses/01_introductory/19_mex_grid/main.hs
@@ -2,12 +2,13 @@
 
 {-# LANGUAGE LambdaCase #-}
 
-import System.Environment
-import System.Exit
-import System.IO
+-- import System.Environment
+-- import System.Exit
+-- import System.IO
 
-import Data.Foldable
-import Data.List
+-- import Data.Foldable
+-- import Data.List
+import Data.Bits
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
@@ -19,6 +20,9 @@ import qualified Data.Map.Strict as Map
 -- Solution to CSES Introductory Problems Mex Grid Construction
 -- https://cses.fi/problemset/task/3419
 
+-- Interesting... https://oeis.org/A003987
+-- https://oeis.org/A004442 Nimsum n+1
+-- https://oeis.org/A004443 Nimsum n+2
 
 -- Your task is to construct an n \times n grid where each square has the smallest nonnegative integer that does not appear to the left on the same row or above on the same column.
 -- Input
@@ -69,18 +73,27 @@ squareM m i j | i == j = (0,m)
               | otherwise =
                   case Map.lookup (i,j) m of
                     Just v -> (v,m)
-                    Nothing -> let (as,m')  = foldr (\ i' (acc,m) -> case squareM m i' j  of (v,mm) -> (v:acc,mm))
+                    Nothing -> let (as,m')  = foldr (\ i' (acc,mm) -> case squareM mm i' j of (v,updated) -> (v:acc,updated))
                                                     ([],m) (reverse [0..i-1])
-                                   (bs,m'') = foldr (\ j' (acc,m) -> case squareM m' i j' of (v,mm) -> (v:acc,mm))
+                                   (bs,m'') = foldr (\ j' (acc,mm) -> case squareM mm i j' of (v,updated) -> (v:acc,updated))
                                                     (as,m') (reverse [0..j-1])
-                                   v = head $ filter ((flip notElem) bs) [1..]
-                               in (v, Map.insert (i,j) v m'')
+                                   sol = head $ filter ((flip notElem) bs) [1..]
+                               in (sol, Map.insert (i,j) sol m'')
 
 solveM :: Int -> [[Int]]
 solveM n = let m = snd $ squareM Map.empty n n -- populate solution map
            in [[fst $ squareM m i j | j <- [0..n-1]] | i <- [0..n-1]]
 
 
+-- OK, after doing this the dumb and slow way
+-- I noticed by pasting some rows of the matrix into OEIS
+-- that they are Nimsums, XORs of row and column.
+-- Nicely played, CSES...
+
+solveXor :: Int -> [[Int]]
+solveXor n = [[i `xor` j | j <- [0..n-1]] | i <- [0..n-1]]
+
+
 
 main :: IO ()
-main = getLine >>= (\ input -> sequence_ $ fmap putStrLn (map (\r -> unwords $ map show r) (solveM $ read input)))
+main = getLine >>= (\ input -> sequence_ $ fmap putStrLn (map (\r -> unwords $ map show r) (solveXor $ read input)))
