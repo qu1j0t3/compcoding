06b10d163d33652bd8aac7d86428c309ef580da5
Toby Thain <toby.thain@fivebox.com>
2026-05-03 01:21:31 -0400

    Memoised solution is faster but still too slow.

--- cses/01_introductory/19_mex_grid/main.hs
+++ cses/01_introductory/19_mex_grid/main.hs
@@ -8,6 +8,9 @@ import System.IO
 
 import Data.Foldable
 import Data.List
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
 -- import Data.Maybe
 -- import Data.IntSet (IntSet, fromList)
 -- import qualified Data.IntSet as IntSet
@@ -38,6 +41,8 @@ import Data.List
 -- 4 5 6 7 0
 
 
+-- In ghci, this starts to get pretty show around order 11
+
 square :: Int -> Int -> Int
 square 0 j = j
 square i 0 = i
@@ -48,10 +53,34 @@ square i j | i == j = 0
                   v = head (filter (\ e -> notElem e filled) [1..])
               in v
 
-
 solve :: Int -> [[Int]]
 solve n = [[square i j | j <- [0..n-1]] | i <- [0..n-1]]
 
 
+type SolutionMap = Map (Int,Int) Int
+
+-- |Memoising evaluator. If solution is in map, return it, and unchanged map.
+-- |otherwise, call self for subproblems and return solution with updated map.
+squareM :: SolutionMap -> Int -> Int -> (Int,SolutionMap)
+squareM m 0 j = (j,m)
+squareM m i 0 = (i,m)
+squareM m i j | i == j = (0,m)
+              | i > j = squareM m j i
+              | otherwise =
+                  case Map.lookup (i,j) m of
+                    Just v -> (v,m)
+                    Nothing -> let (as,m')  = foldr (\ i' (acc,m) -> case squareM m i' j  of (v,mm) -> (v:acc,mm))
+                                                    ([],m) (reverse [0..i-1])
+                                   (bs,m'') = foldr (\ j' (acc,m) -> case squareM m' i j' of (v,mm) -> (v:acc,mm))
+                                                    (as,m') (reverse [0..j-1])
+                                   v = head $ filter ((flip notElem) bs) [1..]
+                               in (v, Map.insert (i,j) v m'')
+
+solveM :: Int -> [[Int]]
+solveM n = let m = snd $ squareM Map.empty n n -- populate solution map
+           in [[fst $ squareM m i j | j <- [0..n-1]] | i <- [0..n-1]]
+
+
+
 main :: IO ()
-main = getLine >>= (\ input -> sequence_ $ fmap putStrLn (map (\r -> unwords $ map show r) (solve $ read input)))
+main = getLine >>= (\ input -> sequence_ $ fmap putStrLn (map (\r -> unwords $ map show r) (solveM $ read input)))
